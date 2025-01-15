1b39867444632b282d59a6743358ad07fe7c8f80
Zhao Wei Liew <zhaoweiliew@gmail.com>
2025-01-15 17:43:57 +0800

    Add echo command

--- src/main/java/Peepo.java
+++ src/main/java/Peepo.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Peepo {
+  private static final String LINE = "    ____________________________________________________________";
+  private static final String INDENT = "     ";
+
   public static void main(String[] args) {
-    System.out.println("____________________________________________________________");
-    System.out.println("Hello! I'm Peepo.\nWhat can I do for you?");
-    System.out.println("____________________________________________________________");
-    System.out.println("Bye. Hope to see you again soon!");
-    System.out.println("____________________________________________________________");
+    System.out.println(LINE);
+    System.out.println(INDENT + "Hi! I'm Peepo.");
+    System.out.println(INDENT + "What can I do for you?");
+    System.out.println(LINE + '\n');
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      for (String input = scanner.nextLine(); !"bye".equals(input); input = scanner.nextLine()) {
+        System.out.println(LINE);
+        System.out.println(INDENT + input);
+        System.out.println(LINE + '\n');
+      }
+    }
+    System.out.println(LINE);
+    System.out.println(INDENT + "Bye. Hope to see you again soon!");
+    System.out.println(LINE);
   }
 }
