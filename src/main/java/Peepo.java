b5ef5c11da455372b056ee85b99dad291f509d9e
Zhao Wei Liew <zhaoweiliew@gmail.com>
2025-01-15 17:34:31 +0800

    Rename to Peepo; greet the user and exit

---
+++ src/main/java/Peepo.java
@@ -0,0 +1,9 @@
+public class Peepo {
+  public static void main(String[] args) {
+    System.out.println("____________________________________________________________");
+    System.out.println("Hello! I'm Peepo.\nWhat can I do for you?");
+    System.out.println("____________________________________________________________");
+    System.out.println("Bye. Hope to see you again soon!");
+    System.out.println("____________________________________________________________");
+  }
+}
